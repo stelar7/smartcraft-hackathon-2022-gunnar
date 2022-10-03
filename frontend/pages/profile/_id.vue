2e54515c4fa3dbd4ec7c50e51e57dba5efe15d57
stelar7 <dudedbz@gmail.com>
2022-10-03 15:07:59 +0200

    add basic fe page

---
+++ frontend/pages/profile/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hello {{ $route.params.id }} hello</div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  async asyncData({ $axios }) {
+    // Fetch any BE data here, then return it as an object to be merged into data();
+    const ip = await $axios.$get('http://icanhazip.com');
+    return { ip };
+  },
+};
+</script>
+
+<style></style>
