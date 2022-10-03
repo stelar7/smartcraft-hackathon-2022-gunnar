d4e7792d0a35322bd44c35352c7d44454343e0b2
emilia-m-k <emi.kamrowska@gmail.com>
2022-10-03 18:27:04 +0200

    Game over page and some more changes to main page

--- frontend/pages/main/index.vue
+++ frontend/pages/main/index.vue
@@ -1,5 +1,15 @@
 <template>
   <v-container>
-    <v-card>current task</v-card>
+    <h3>Current task</h3>
+    <v-card min-height="200" class="mb-6">
+      <v-card-title class="secondary"> Fix that pipe </v-card-title>
+      <v-card-text></v-card-text>
+    </v-card>
+
+    <h3 class="mt-6" >Next</h3>
+    <v-card min-height="200">
+      <v-card-title class="secondary"> Fix some other pipe </v-card-title>
+      <v-card-text></v-card-text>
+    </v-card>
   </v-container>
 </template>
