af41c352ea7633145d6f51fc2e10de73792390b7
emilia-m-k <emi.kamrowska@gmail.com>
2022-10-04 10:35:53 +0200

    Icons added to layout

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -6,9 +6,11 @@
       <v-spacer />
       <div class="user d-flex flex-row">
         <div class=monthly-score>
+            <fa-icon :icon="['fa', 'trophy']" class="amber--text"/>
             {{ user.ThisMonthsScore }}
         </div>
         <div>
+          <fa-icon :icon="['fa', 'user']" class=""/>
           {{ user.Name }}
         </div>
       </div>
