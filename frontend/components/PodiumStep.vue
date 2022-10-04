384a8c70ad01b40de154ba73ce6757094af966e4
Martine Jacobsen <martine.jacobsen@cordel.no>
2022-10-04 09:06:50 +0200

    scoreboard page, table component and podium component

---
+++ frontend/components/PodiumStep.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="step-container">
+        <p class="avatar">{{step}}</p>
+        <div :class="[styleForStep, 'step']">
+            {{step}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PodiumStep',
+  props: ["step"],
+  computed: {
+    styleForStep() {
+        return "step" + this.step;
+    }
+  }
+};
+</script>
+
+<style>
+    .step-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 50px;
+        width: 100%;
+        padding: 0 5px;
+    }
+    .step {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        padding-top: 10px;
+        box-sizing: border-box;
+    }
+    .step1 {
+        height: 150px;
+        background-color: goldenrod;
+    }
+    .step2 {
+        height: 100px;
+        background-color: silver;
+    }
+    .step3 {
+        height: 75px;
+        background-color: rgb(165, 88, 44);
+    }
+    .avatar {
+        width: 20vw;
+        height: 20vw;
+        border-radius: 100%;
+        background-color: lightgreen;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
---
+++ frontend/components/ScoreTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="scores"
+    :items-per-page="5"
+    class="elevation-3"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'ScoreTable',
+  props: ["scores"],
+  computed: {
+    headers() {
+        return [
+            {
+                text: "name",
+                value: "name"
+            },
+            {
+                text: "score",
+                value: "score",
+            }
+        ]
+    },
+  }
+};
+</script>
