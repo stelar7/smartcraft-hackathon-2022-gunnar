fd96a6b7b6080c55557bbff2cf238c20ee1f409c
emilia-m-k <emilia-m-k@users.noreply.github.com>
2022-10-04 07:12:43 +0000

    deploy: 5e4e9dde2e80b0978848ec56ea5eb641b029fa5e

---
+++ _nuxt/62773fb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{509:function(e,n,t){"use strict";t.r(n);var l={name:"IndexPage"},o=t(68),component=Object(o.a)(l,(function(){return(0,this._self._c)("div",[this._v("hello this is the index page :)")])}),[],!1,null,null,null);n.default=component.exports}}]);
