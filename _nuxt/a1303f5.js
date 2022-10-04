40063c22c6bdeb468fe466da51845d8005bacb64
MartineJacobsen <MartineJacobsen@users.noreply.github.com>
2022-10-04 08:37:06 +0000

    deploy: 6199a8c891525168d869468e0044b4463fd6f55c

--- _nuxt/a1303f5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{526:function(e,n,t){"use strict";t.r(n);var l={name:"IndexPage"},o=t(70),component=Object(o.a)(l,(function(){return(0,this._self._c)("div",[this._v("hello this is the index page :)")])}),[],!1,null,null,null);n.default=component.exports}}]);
