2b7bf4c9a69028157c4edf6f958037f233f9b529
emilia-m-k <emi.kamrowska@gmail.com>
2022-10-04 09:09:11 +0200

    Empty layout added and used i logout

---
+++ frontend/layouts/empty.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <Nuxt class="app" />
+  </v-app>
+</template>
+
+<style lang="scss" scoped>
+.app {
+  height: 100%;
+}
+</style>
--- frontend/pages/logout/index.vue
+++ frontend/pages/logout/index.vue
@@ -3,6 +3,13 @@
     <h1 class="accent--text text-center d-flex justify-center">GAME OVER</h1>
   </div>
 </template>
+
+<script>
+export default {
+  layout: 'empty',
+};
+</script>
+
 <style scoped>
 h1 {
   font-size: 72px;
