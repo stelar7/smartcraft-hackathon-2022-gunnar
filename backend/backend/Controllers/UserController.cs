2d50e859900738f1de34611cee0fd12bdc1c5e4b
Steinar Roald <steinar.roald@cordel.no>
2022-10-04 12:16:17 +0200

    Get yearly, monthly and weekly leaders

--- backend/backend/Controllers/UserController.cs
+++ backend/backend/Controllers/UserController.cs
@@ -26,5 +26,26 @@ namespace backend.Controllers
         {
             return await HandleRequest(async () => await repository.GetRecords());
         }
+
+        [HttpGet("YearlyLeaders")]
+        public async Task<ActionResult<List<User>>> GetYearlyLeaders()
+        {
+            return await HandleRequest(async () => (await repository.GetRecords())
+                .OrderByDescending(u => u.ThisYearsScore).ToList());
+        }
+
+        [HttpGet("MonthlyLeaders")]
+        public async Task<ActionResult<List<User>>> GetMonthlyLeaders()
+        {
+            return await HandleRequest(async () => (await repository.GetRecords())
+                .OrderByDescending(u => u.ThisMonthsScore).ToList());
+        }
+
+        [HttpGet("WeeklyLeaders")]
+        public async Task<ActionResult<List<User>>> GetWeeklyLeaders()
+        {
+            return await HandleRequest(async () => (await repository.GetRecords())
+                .OrderByDescending(u => u.ThisWeeksScore).ToList());
+        }
     }
 }
